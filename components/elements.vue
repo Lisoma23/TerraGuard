4f5f887aa4cd5652eeb9b64288db3deb53a66f35
Sofia <115009629+Lisoma23@users.noreply.github.com>
2024-04-29 11:10:54 +0200

    Add Project

---
+++ components/elements.vue
@@ -0,0 +1,105 @@
+<template>
+  <div :id="nomCat">
+    <div id="bad">
+      <template v-for="(element, index) in elementsListe">
+        <p :id="element.statut + index" v-if="element.statut == 'bad'" @click="levelUpBad($event)">
+          {{ element.nom }} <span id="score">0</span>/100
+        </p>
+      </template>
+    </div>
+    <div id="good">
+      <template v-for="(element, index) in elementsListe">
+        <p :id="element.statut + (index - 4)" v-if="element.statut == 'good'" @click="levelUpGood($event)">
+          {{ element.nom }} <span id="score">0</span>/10
+        </p>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    nomCat: {
+      type: String,
+      required: true,
+    },
+    elementsListe: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<script setup>
+import { setmoney, getmoney, redMoney, setProgress, getProgress, setProgressHapiness, getProgressHapiness, setAchat, addScoreDiv } from "../game"
+
+const levelUpGood = (event) => {
+  let scoreElementGood;
+  if (event.target.id == "score") {
+    scoreElementGood = event.target;
+  } else {
+    scoreElementGood = event.target.querySelector("#score");
+  }
+  const pIdGood = scoreElementGood.parentElement.id; //recupere l'id de l'element good sur lequel on clique
+  const lastCharacter = pIdGood.slice(-1); //recupere le dernier caractere de l'id (pour trouver le bad correspondant)
+  const goodParent = scoreElementGood.parentElement.parentElement.parentElement; //récupère la div de la catégorie
+  let scoreGood = parseInt(scoreElementGood.textContent); //récupère le score en int du good
+
+  const badElement = goodParent.querySelector("#bad" + lastCharacter); //récupère l'élement bad correspondant dans la catégorie
+  const scoreElementBad = badElement.querySelector("#score"); //récupères la balise span du bad
+  let scoreBad = parseInt(scoreElementBad.textContent); //score en int du bad
+
+  if (scoreBad >= 10 && scoreGood != 10 && getmoney() >= 50) {
+    //update les scores
+    scoreGood += 1;
+    scoreElementGood.textContent = scoreGood;
+    scoreBad -= 10;
+    addScoreDiv(scoreElementGood);
+    setmoney(getmoney() - 50) //retire la somme dépenser pour acheter l'élément
+    setProgress(getProgress() - 5) //retire un peu de pollution
+    setProgressHapiness(getProgressHapiness() + 30) //améliore l'hapiness car on améliore la condition de vie
+    scoreElementBad.textContent = scoreBad;
+    scoreElementBad.style.color = "#00ff00";
+    scoreElementBad.style.fontWeight = "bold";
+    setTimeout(function () { //laisse en vert et gras 700ms avant de repasser en normal
+      scoreElementBad.style.color = "#fff";
+      scoreElementBad.style.fontWeight = "normal";
+    }, 700);
+  } else if (scoreBad < 10 && scoreGood < 10) {
+    scoreElementBad.style.color = "#ff0000";
+    scoreElementBad.style.fontWeight = "bold";
+    setTimeout(function () { //laisse en rouge et gras 700ms avant de repasser en normal
+      scoreElementBad.style.color = "#fff";
+      scoreElementBad.style.fontWeight = "normal";
+    }, 700);
+  } else if (getmoney() < 50) {
+    redMoney(); //rappel au joueur qu'il n'a pas assez
+  }
+};
+
+const levelUpBad = (event) => {
+  //augmente le nombre d'unité que possède l'élément
+  let scoreElement;
+  if (event.target.id == "score") {
+    scoreElement = event.target;
+  } else {
+    scoreElement = event.target.querySelector("#score");
+  }
+  if (getmoney() >= 100) {
+    addScoreDiv(scoreElement);
+    setAchat();
+    setmoney(getmoney() - 100) //retire la somme dépenser pour acheter l'élément
+    let score = parseInt(scoreElement.textContent);
+    if (score != 100) {
+      score += 1;
+      scoreElement.textContent = score;
+    }
+    setProgress(getProgress() + 1) //augmente la valeur de la barre de pollution
+  } else {
+    redMoney(); //indique au joueur qu'il n'a pas assez d'argent
+  }
+
+};
+</script>
---
+++ game.ts
@@ -0,0 +1,89 @@
+import { gsap } from "gsap";
+
+let money: number = 100;
+let moneyDisplay: HTMLElement;
+let progress: number = 0;
+let progressDiv: HTMLElement;
+let progressDivHapiness: HTMLElement;
+let progressHapiness: number = 99;
+let achat: number = 0;
+let scoreDiv:HTMLElement[] = new Array(); 
+
+export function setmoney(value: number) {
+  money = value;
+  moneyDisplay.textContent = money + "$";
+}
+
+export function getmoney() {
+  return money;
+}
+
+export function setMoneyDisplay(Div: HTMLElement) {
+  moneyDisplay = Div;
+}
+
+export function redMoney() {
+  moneyDisplay.style.color = "#ff0000";
+  moneyDisplay.style.fontWeight = "bold";
+  setTimeout(function () {
+    //laisse en rouge et gras 700ms avant de repasser en normal
+    moneyDisplay.style.color = "#fff";
+    moneyDisplay.style.fontWeight = "normal";
+  }, 700);
+}
+
+export function setProgress(value: number) {
+  progress = value;
+  updateProgress(progressDiv, progress);
+}
+
+export function getProgress() {
+  return progress;
+}
+
+function updateProgress(Div: HTMLElement, value : number) {
+  gsap.to(Div, {
+    width: value + "%",
+    duration: 2,
+  });
+}
+
+export function setProgressHapiness(value: number) {
+  progressHapiness = value;
+  updateProgress(progressDivHapiness, progressHapiness);
+}
+
+export function getProgressHapiness() {
+  return progressHapiness;
+}
+
+export function setProgressBarres(DivP: HTMLElement, DivH: HTMLElement){
+  progressDiv = DivP;
+  progressDivHapiness = DivH;
+}
+
+export function setAchat() {
+  achat += 1;
+}
+
+export function reset() {
+  achat = 0;
+  setProgress(0)
+  setProgressHapiness(50)
+  setmoney(100);
+  setScore();
+}
+
+export function getAchat() {
+  return achat;
+}
+
+export function addScoreDiv(Div : HTMLElement){
+  scoreDiv.push(Div);
+}
+
+function setScore() {
+  for (let i = 0; i < scoreDiv.length; i++){
+    scoreDiv[i].textContent = "0";
+  }
+}
